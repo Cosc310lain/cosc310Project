66297c7a343ba41203b2dde5270c550a2b8d759d
Cosc310lain <116384800+Cosc310lain@users.noreply.github.com>
2022-11-14 19:36:11 -0800

    Delete src directory

--- src/InterfacesMoods/HappyInterface.java
+++
@@ -1,8 +0,0 @@
-package InterfacesMoods;
-
-public interface HappyInterface {
-	public static void happyInvoked(String userName) {
-		System.out.println("I'm so glad to hear you are feeling happy! Continue to spread your positivity :)");
-		System.out.println("Thank you for using your our service, " + userName + "!");
-	}
-}
--- src/Moods/Happy.java
+++
@@ -1,8 +0,0 @@
-package Moods;
-
-public class Happy {
-	public void happyInvoked(String userName) {
-		System.out.println("I'm so glad to hear you are feeling happy! Continue to spread your positivity :)");
-		System.out.println("Thank you for using your our service, " + userName + "!");
-	}
-}
